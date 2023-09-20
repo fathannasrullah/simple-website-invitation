fba46c4284ac32192b96d69580c49cff793fb4cb
fathan nasrullah <fathannasrullah0@gmail.com>
2023-09-21 03:36:23 +0700

    create a website invitation

---
+++ README.md
@@ -0,0 +1,19 @@
+## Installation
+1. I used [http-server](https://www.npmjs.com/package/http-server) (a simple static HTTP server) for running website at local development
+
+
+## Javascript Feature
+1. Open Invitation
+   - You can access this feature by clicking the `open invitation` button on the cover page
+
+2. Navigation
+   - This feature is on the invitation page at the very bottom of the page. it only showed when the user scroll up.
+
+3. Image Slideshow
+   - This feature is the background for the home section on the invitation page by applying image slideshow
+
+4. Transition Scroll Animation
+   - This feature is in the couple section on the invitation page. The animation effect is showed when the user scroll down from the home section to the couple section
+
+5. Create new HTML element with DOM
+   - This feature is used by utilizing the DOM. So there is no need to rewrite HTML code that has the same content
---
+++ index.js
@@ -0,0 +1,143 @@
+const cover = document.querySelector('main .cover-container')
+const invitation = document.querySelector('main .invitation-container')
+const openInvitationBtn = document.getElementById('openInvitationBtn')
+const navigation = document.querySelector('main aside .navigation')
+const footer = document.querySelector('.footer')
+const music = document.querySelector('.music')
+const audio = document.querySelector('audio');
+
+// handle open-invitation button click
+openInvitationBtn.onclick = () => {
+  cover.style.display = 'none'
+  invitation.style.display = 'flex'
+  navigation.style.display = 'block'
+  footer.style.display = 'flex'
+  music.style.display = 'block'
+  audio.volume = 0.2
+  audio.play()
+}
+
+// handle hide navigation based on scroll
+let prevScrollposition = window.scrollY
+window.onscroll = () => {
+  const currentScrollPosition = window.scrollY
+
+  if (prevScrollposition > currentScrollPosition) {
+    navigation.style.bottom = '0'
+  } else {
+    navigation.style.bottom = '-70px'
+  }
+
+  prevScrollposition = currentScrollPosition
+}
+
+// handle image slideshow at home section
+// resource -> https://github.com/PiusLucky/Image-Slider
+let slides = document.querySelectorAll('#all_slides .slide')
+let currentSlide = 0
+
+const goToSlide = (s) => {
+  slides[currentSlide].className = 'slide'
+  
+  currentSlide = (s + slides.length) % slides.length
+
+  slides[currentSlide].className = 'slide active'
+}
+
+const nextSlide = () => {
+  goToSlide(currentSlide + 1)
+}
+
+let slideInterval = setInterval(nextSlide, 2100)
+
+const playSlideshow = () => {
+  slideInterval = setInterval(nextSlide, 2100)
+}
+
+playSlideshow()
+
+// handle show couple based on scroll
+// resource -> https://codepen.io/alvarotrigo/pen/PoKamZy
+const groom = document.querySelector('.couple .groom')
+const bride = document.querySelector('.couple .bride')
+
+function showCouple() {
+  const windowHeight = window.innerHeight
+  const elementGroomTop = groom.getBoundingClientRect().top
+  const elementBrideTop = bride.getBoundingClientRect().top
+  const elementVisible = 150;
+
+  if ((elementGroomTop || elementBrideTop) < windowHeight - elementVisible) {
+    groom.classList.add('active')
+    bride.classList.add('active')
+  }
+}
+
+window.addEventListener('scroll', showCouple)
+
+
+// create new element with javascript dom manipulation
+const wishesList = [
+  {
+    name: 'fulan',
+    wishes: 'selamat ya..'
+  },
+  {
+    name: 'fulanah',
+    wishes: 'selmat kak.. semoga jadi keluarga yang samawa..'
+  },
+  {
+    name: 'fulan',
+    wishes: 'congrats ya.. semoga langgeng dan punya anak yang solih solihah. Jadi keluarga yang sakinah mawaddah warohmah'
+  },
+  {
+    name: 'fulanah',
+    wishes: 'selmat kak.. semoga jadi keluarga yang samawa..'
+  },
+  {
+    name: 'fulan',
+    wishes: 'selamat ya..'
+  },
+  {
+    name: 'fulanah',
+    wishes: 'selmat kak.. semoga jadi keluarga yang samawa..'
+  }
+]
+
+const wishesListElmt = document.querySelector('.wishes-list')
+
+wishesList.map(({ name, wishes }) => {
+  const wishesItemElmt = document.createElement('section')
+  wishesItemElmt.classList.add('wishes-item')
+  wishesItemElmt.innerHTML = `
+    <div class='wishes-item-avatar'>
+      <i class="fas fa-user"></i>
+    </div>
+    <div class='wishes-item-content'>
+      <h6>${name}</h6>
+      <p>${wishes}</p>
+    </div>
+  `
+
+  wishesListElmt.append(wishesItemElmt)
+})
+
+// handle playing music
+// resource -> https://codepen.io/freeCodeCamp/pen/MWyBapv
+const musicBtn = document.querySelector('.music button');
+const musicIcon = document.querySelector('.music button i');
+
+musicBtn.onclick = () => {
+  if (audio.paused) {
+    audio.volume = 0.2
+    audio.play()
+    musicBtn.classList.add('music-playing')
+    musicIcon.classList.remove('fa-play')
+    musicIcon.classList.add('fa-pause')
+  } else {
+    audio.pause()
+    musicBtn.classList.remove('music-playing')
+    musicIcon.classList.remove('fa-pause')
+    musicIcon.classList.add('fa-play')
+  }
+}
